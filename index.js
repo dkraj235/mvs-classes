b4ea6289b2f28b74ed523c20a051d06c95f19499
Dilip <dilipram06228@gmail.com>
2023-10-04 09:52:44 +0530

    3rd commit

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
+ 
+ 
 
 const app = express();
 const port = process.env.PORT || 3000;
+const dbURI = process.env.DB_URI;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect('mongodb+srv://mvsclasses:mvsclasses80029@mvsclasses.i8rkiop.mongodb.net/?appName=mongosh+1.6.1', {
+mongoose.connect(dbURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
